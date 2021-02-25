03f4dd3a40d901c4d77ec802c31b89097ea59242
KelinSasha <KelinSashaACWG@gmail.com>
2021-02-25 11:08:16 -0800

    Update

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'r4NnhH8p2DXQOJ2F1IjUfJG4yUMoOAlg';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+ 		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	var marker = L.marker([32.88, -117.236]).addTo(map);
 }
